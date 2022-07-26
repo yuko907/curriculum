5bc41565d302180888616fa3bfb71b09ea7883e8
yuko907 <103582206+yuko907@users.noreply.github.com>
2022-07-27 00:04:16 +0900

    課題提出テスト
    3章チェックテストの提出

---
+++ 3-1/Main.java
@@ -0,0 +1,40 @@
+/**
+ * バブルソート
+ * チェックテスト-Java３章
+ *
+ */
+public class Main{
+    public static void main(String[] args){
+        /*
+        * 問1
+        * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+        */
+
+        int[] data = {3,1,2,7,5};
+
+        /*
+        * 問2
+        * 以下のfor文を完成させなさい
+        */
+        for(int i = 0; i < 5 ; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 4 ; j > i; j--) {
+                /*
+                * 問3
+                * 以下、配列の添字を入れてソートを完成させなさい
+                */
+                if(data[i] > data[j]){
+                  int box = data[i];
+                  data[i] = data[j];
+                  data[j] = box;
+                }
+            }
+        }
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+    }
+}
